a895c07fd1770480e55006689c8e576c0fac5b73
Dwayne Johnson <47540825+Sachinxcraze@users.noreply.github.com>
2022-03-10 16:36:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+### Installation
+
+#### The Easiest Way
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+#### The Hard Way
+
+```sh
+virtualenv -p python3 VENV
+. ./VENV/bin/activate
+pip install -r requirements.txt
+cp sample_config.py config.py
+--- EDIT config.py values appropriately ---
+python bot.py
+```
+
+
+
+#### LICENSE
+- GPLv3
---
+++ translation.py
@@ -0,0 +1,86 @@
+class Translation(object):
+    START_TEXT = """🙋‍♂ʜᴇʏ, {} 
+ᴛʜɪꜱ ɪꜱ ᴀ ᴛᴇʟᴇɢʀᴀᴍ ꜰɪʟᴇ ᴜʀʟ ᴜᴘʟᴏᴀᴅ ʙᴏᴛ!🤖
+
+🔗 ᴘʟᴇᴀꜱᴇ ꜱᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ ᴜʀʟ, ɪ ᴡɪʟʟ ᴜᴘʟᴏᴀᴅ ɪᴛ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀꜱ ꜰɪʟᴇ..
+
+
+"""
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = " <b>ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ᴀᴛ ɴᴏᴡ</b> "
+    FORMAT_SELECTION = " <b>ᴄʜᴏᴏꜱᴇ ᴛʜᴇ ꜰᴏʀᴍᴀᴛ👇</b> <a href='{}'>"
+    SET_CUSTOM_USERNAME_PASSWORD = """
+"""
+    NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    DOWNLOAD_START = " <b>📥ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛ ɪꜱ ᴘʟᴀᴄᴇᴅ ɪɴ ᴛʜᴇ Qᴜᴇᴜᴇ. ᴘʟᴇᴀꜱᴇ ᴅᴏ ɴᴏᴛ ꜱᴇɴᴅ ᴀɴᴏᴛʜᴇʀ ʀᴇQᴜᴇꜱᴛ. ʙᴇ ᴘᴀᴛɪᴇɴᴛ ...</b>  "
+    UPLOAD_START = " <b>📤ᴜᴘʟᴏᴀᴅɪɴɢ</b> <b>ꜱᴛᴀʀᴛᴇᴅ...</b> "
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry! I can't upload files that are larger than 1.95GB ."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks For Using Me"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "<b>ꜰɪʟᴇ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴜᴘʟᴏᴀᴅᴇᴅ</b>"
+    NOT_AUTH_USER_TEXT = "your Not A Vip subscriber ."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = " <b>ᴄᴜꜱᴛᴏᴍ</b> <b>ᴛʜᴜᴍʙɴᴀɪʟ</b> <b>ꜱᴀᴠᴇᴅ</b> "
+    DEL_ETED_CUSTOM_THUMB_NAIL = " <b>ᴛʜᴜᴍʙɴᴀɪʟ</b> <b>ᴅᴇʟᴇᴛᴇᴅ</b> <b>ꜱᴜᴄᴄᴇꜱꜰᴜʟʟʏ</b> "
+    ABOUT_ME = "ɴᴏᴛ ᴀᴄᴄᴇᴘᴛɪɴɢ ɴᴏᴡ"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    NO_VOID_FORMAT_FOUND =" <b>❌ᴜɴꜱᴜᴘᴘᴏʀᴛᴇᴅ ᴜʀʟ ᴏʀ ᴇʀʀᴏʀ❌</b> "
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    CURENT_PLAN_DETAILS = """
+ <b>ɴᴏᴛ ᴀᴄᴄᴇᴘᴛɪɴɢ ɴᴏᴡ</b>
+
+ 
+
+
+"""
+    HELP_USER = """🤖 ɪ ᴄᴀɴ ᴜᴘʟᴏᴀᴅ ᴛᴇʟᴇɢʀᴀᴍ ꜰɪʟᴇꜱ:
+ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜᴘʟᴏᴀᴅ ꜰɪʟᴇ ʙʏ ᴜʀʟ ᴊᴜꜱᴛ ᴛᴏ ꜱᴇɴᴛ ᴀ ᴠᴀʟɪᴅ ᴅɪʀᴇᴄᴛ ᴜʀʟ ɪ ᴡɪʟʟ ᴜᴘʟᴏᴀᴅ ɪᴛ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ.
+
+🔘 ᴅᴏ ʏᴏᴜ ɴᴇᴇᴅ ᴀ ᴄᴜꜱᴛᴏᴍ ꜰɪʟᴇɴᴀᴍᴇ ꜰᴏʟʟᴏᴡ ᴛʜɪꜱ ꜰᴏʀᴍᴀᴛ ᴇxᴀᴍᴘʟᴇ👇
+http://www.example.com|example.apk
+
+🔘 ʏᴏᴜ ɴᴇᴇᴅ ᴀ ᴛʜᴜᴍʙɴᴀɪʟ ᴊᴜꜱᴛ ꜱᴇɴᴛ ᴍᴇ ᴀɴ ɪᴍᴀɢᴇ.
+
+🔘 ʏᴏᴜʀ ᴅᴏᴡɴʟᴏᴀᴅ ꜱᴘᴇᴇᴅ ᴡɪʟʟ ᴅᴇᴘᴇɴᴅ ᴏɴ ʏᴏᴜʀ ʟɪɴᴋ ꜱᴘᴇᴇᴅ.
+
+🔘 ʏᴏᴜᴛᴜʙᴇ_ᴅʟ ꜱᴜᴘᴘᴏʀᴛᴇᴅ.
+
+
+<a href="https://t.me/XCRobots_bot_updates/9">[ɪꜰ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ᴅᴏᴜʙᴛꜱ, ᴡᴀᴛᴄʜ ᴛᴜᴛᴏʀɪᴀʟ ꜰᴏʀ ᴄʟɪᴄᴋ ʜᴇʀᴇ]</a>
+
+📮 ɪꜰ ʏᴏᴜ ꜰɪɴᴅ ᴀɴʏ ʙᴜɢꜱ ʀᴇᴘᴏʀᴛ @Edwardnowden
+
+"""
+ 
+    REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
+    REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
+    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @AnyDLBot"
+    FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
+    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
+    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "A saved media already exists. Please send /storageinfo to know the current media details."
+    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams"
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
+    INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
+    BANNED_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
+    EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
+    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
+    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
+You can use /rename command after receiving file to rename it with custom thumbnail support."""
+    CANCEL_STR = "Process Cancelled"
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 30 minutes.
+/upgrade or Try 1800 seconds later."""
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
